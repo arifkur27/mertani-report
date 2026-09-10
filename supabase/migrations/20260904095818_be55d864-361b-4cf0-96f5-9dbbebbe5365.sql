a631a3c298dbcd0de0a3d9ba145413d778599a1b
arifkur27 <arifkurniawan47203@gmail.com>
2026-09-10 22:51:13 +0700

    Kosongkan project

--- supabase/migrations/20260904095818_be55d864-361b-4cf0-96f5-9dbbebbe5365.sql
+++
@@ -1,5 +0,0 @@
-REVOKE ALL ON FUNCTION public.has_role(uuid, public.app_role) FROM PUBLIC, anon;
-REVOKE ALL ON FUNCTION public.my_divisi() FROM PUBLIC, anon;
-REVOKE ALL ON FUNCTION public.handle_new_user() FROM PUBLIC, anon, authenticated;
-GRANT EXECUTE ON FUNCTION public.has_role(uuid, public.app_role) TO authenticated, service_role;
-GRANT EXECUTE ON FUNCTION public.my_divisi() TO authenticated, service_role;
--- supabase/migrations/20260904095849_ef826dbd-543d-443d-9247-8b6bd0264f59.sql
+++
@@ -1,6 +0,0 @@
-CREATE POLICY "report_photos_read" ON storage.objects FOR SELECT TO authenticated
-  USING (bucket_id = 'report-photos');
-CREATE POLICY "report_photos_insert" ON storage.objects FOR INSERT TO authenticated
-  WITH CHECK (bucket_id = 'report-photos' AND (storage.foldername(name))[1] = auth.uid()::text);
-CREATE POLICY "report_photos_delete" ON storage.objects FOR DELETE TO authenticated
-  USING (bucket_id = 'report-photos' AND (storage.foldername(name))[1] = auth.uid()::text);
